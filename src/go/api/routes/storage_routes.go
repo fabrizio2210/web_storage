782eab396a72993c0ba1eae552a8ea6a94f5765e
Fabrizio W <fabrizio2210@gamil.com>
2024-05-06 18:41:22 +0100

    Accept all the slashes for the object

--- src/go/api/routes/storage_routes.go
+++ src/go/api/routes/storage_routes.go
@@ -8,6 +8,6 @@ import (
 
 func StorageRoute(router *gin.Engine) {
   // router.DELETE("/api/photo/:photoId", controllers.DeleteObject())
-  router.GET(   "/storage/:objectId", controllers.GetObject())
-  router.POST(  "/storage/:objectId",      controllers.PostNewObject())
+  router.GET(   "/storage/*objectId", controllers.GetObject())
+  router.POST(  "/storage/*objectId",      controllers.PostNewObject())
 }
